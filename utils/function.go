c2d91a57827c3d6252b6489446588871590fea08
shahadathhs <shahadathhossensajib732@gmail.com>
2025-05-09 16:49:06 +0600

    feat: function to take user input from console

--- utils/function.go
+++ utils/function.go
@@ -2,19 +2,60 @@ package utils
 
 import "fmt"
 
-func Function() {
-	// Welcome Message
+func welcomeMessage() {
 	fmt.Println("Welcome to function in utils package")
+}
 
-	// Get User Input
+func getUserFullName() string {
 	var name string
-	fmt.Println("Please enter your name")
-	fmt.Scanln(&name)
-	fmt.Println("Hello", name)
+	var firstName string
+	var lastName string
+	fmt.Println("Please enter your first name")
+	fmt.Scanln(&firstName)
+	fmt.Println("Please enter your last name")
+	fmt.Scanln(&lastName)
+	name = firstName + " " + lastName
+	return name
+}
 
-	// Get User Input
+func getUserAge () int {
 	var age int
 	fmt.Println("Please enter your age")
 	fmt.Scanln(&age)
-	fmt.Println("You are", age, "years old")
+	return age
+}
+
+func getUserAddress () string {
+	var address string
+	fmt.Println("Please enter your address")
+	fmt.Scanln(&address)
+	return address
+}
+
+func printUserData(name string, age int, address string) {
+	fmt.Println("Here is your provided data")
+	fmt.Println("Your full name is", name)
+	fmt.Println("Your age is", age)
+	fmt.Println("Your address is", address)
+}
+
+func goodbyeMessage() {
+	fmt.Println("Thank you for using our service, Goodbye")
+}
+
+func Function() {
+	welcomeMessage()
+	
+	name := getUserFullName()
+	fmt.Println("Your full name is", name)
+
+	age := getUserAge()
+	fmt.Println("Your age is", age)
+
+	address := getUserAddress()
+	fmt.Println("Your address is", address)
+	
+	printUserData(name, age, address)
+
+	goodbyeMessage()
 }
