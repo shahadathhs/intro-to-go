b6908b7c8a904535193baaf465e530e8646d0765
shahadathhs <shahadathhossensajib732@gmail.com>
2025-05-09 20:24:15 +0600

    feat: exploered init function

--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
-import "intro-to-go/utils"
+import (
+	"fmt"
+)
+
+var a = 1
 
 func main() {
 	// utils.Intro()
 	// utils.Function()
-	utils.Scope()
+	// utils.Scope()
+
+	// age := 20
+
+	// if age >= 18 {
+	// 	a := 3
+	// 	fmt.Println(a)
+	// }
+
+	fmt.Println("Main Function called", a)
+}
+
+func init() {
+	fmt.Println("init function called", a)
+	a := 2
+	fmt.Println(a)
 }
