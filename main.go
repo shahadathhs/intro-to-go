83352ec6fbf31ee2397eb7ab2b52bba6321f393c
shahadathhs <shahadathhossensajib732@gmail.com>
2025-05-10 15:11:07 +0600

    feat: explored closure

--- main.go
+++ main.go
@@ -1,18 +1,26 @@
 package main
 
-import (
-	"intro-to-go/utils"
-)
+import "fmt"
+
+// import (
+// 	"intro-to-go/utils"
+// )
 
 var a = 1
+
+func add(x int, y int) {
+	z := x + y
+	fmt.Println(z)
+}
 
 func main() {
 	// utils.Intro()
 	// utils.Function()
 	// utils.Scope()
-	utils.TypesOfFunction()
+	// utils.TypesOfFunction()
 
-	// fmt.Println("Main Function called", a)
+	add(2, 3)
+	add(a, 2)
 }
 
 // func init() {
